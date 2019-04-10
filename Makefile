6c4f4f4bfd6d511380568ad799b0ce66070e3aca
Wichert Akkerman <wichert@wiggy.net>
2019-04-10 11:12:31 +0200

    Initial commit

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-.PHONY: setup test clean bundle start stop restart size bundlesize
+.PHONY: setup lint test test-ci clean
 
 setup:
 	yarn
@@ -18,37 +18,9 @@ test-ci:
 	./node_modules/.bin/lerna bootstrap
 	./node_modules/.bin/ava
 	make lint
-	make restart
-	./node_modules/.bin/lerna run prepublish
-	node bundle.js
-	./node_modules/.bin/ava --serial --fail-fast test/
-	make bundlesize
 
 clean:
-	make stop
-	rm -f prosody/prosody.err
-	rm -f prosody/prosody.log
-	rm -f prosody/prosody.pid
 	./node_modules/.bin/lerna clean --yes
 	rm -rf node_modules/
 	rm -f packages/*/dist/*.js
 	rm -f lerna-debug.log
-
-start:
-	./server/ctl.js start
-
-stop:
-	./server/ctl.js stop
-
-restart:
-	./server/ctl.js restart
-
-bundlesize:
-	./node_modules/.bin/bundlesize
-
-bundle:
-	node bundle.js
-
-size:
-	make bundle
-	make bundlesize
---
+++ packages/vcard/index.js
@@ -0,0 +1,56 @@
+'use strict'
+
+const {xml} = require('@xmpp/client')
+
+const NS = 'vcard-temp'
+
+function parse({children}) {
+  return children.reduce((dict, c) => {
+    dict[c.name] =
+      c.children && typeof c.children[0] === 'string' ? c.text() : parse(c)
+    return dict
+  }, {})
+}
+
+function build(dict, parent) {
+  return (parent || xml('vCard', {xmlns: NS})).append(
+    Object.entries(dict).map(([key, val]) => {
+      return typeof val === 'object' ? build(val, xml(key)) : xml(key, {}, val)
+    })
+  )
+}
+
+/**
+ * Register a vcard plugin.
+ *
+ * @param {Client} client XMPP client instance
+ * @returns {VcardPlugin} Vcard plugin instance
+ */
+function setupVcard({iqCaller}) {
+  return {
+    /**
+     * Request the vcard of a user. If no jid is provided the vcard for
+     * the current user is returned.
+     *
+     * @param {string|undefined} jid Jabber id whose vcard we want
+     * @returns {Promise<object>} vcard instance
+     */
+    get(jid) {
+      return iqCaller
+        .request(xml('iq', {type: 'get'}, xml('vCard', {xmlns: NS}, jid)))
+        .then(r => parse(r).vCard)
+    },
+
+    /**
+     * Change the user's vcard
+     *
+     * @param {object} vCard Vcard data to store
+     * @returns {Promise<void>} Completion promise
+     */
+    set(vCard) {
+      return iqCaller.request(xml('iq', {type: 'set'}, build(vCard)))
+    },
+  }
+}
+
+module.exports = setupVcard
